ee2bbd07526039d3bc8a7ef3b3abe3e1e8efa0b7
Oliver Eidel <oli@eidel.net>
2020-03-21 23:49:25 +0100

    fix distinct constraint and ordering in sliding window query

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -9,23 +9,27 @@ class Questionnaire < ApplicationRecord
   self.implicit_order_column = "created_at"
 
   def self.sliding_window
+    # Ordering could be not returning the newest entry per client in time window
     Rails.cache.fetch("questionnaires/sliding_window", expires_in: 5.minutes) do
       select("DISTINCT ON (client_uuid) client_uuid, *")
         .where("created_at > ?", Rails.configuration.sliding_window.ago)
+        .order("client_uuid, created_at DESC")
     end
   end
 
   def nearby_sick_percentage(radius_km:)
+    # Using .length because .count is currently broken due to the
+    # distinct constraint in the sliding window
     sick = self.class.sliding_window
              .near([latitude, longitude], radius_km, units: :km)
              .where(healthy: false)
              .where.not(id: id)
-             .count(:all)
+             .length
     healthy = self.class.sliding_window
                 .near([latitude, longitude], radius_km, units: :km)
                 .where(healthy: true)
                 .where.not(id: id)
-                .count(:all)
+                .length
 
     total = healthy + sick
 
