e297a75ab7d2844c118371fd5cfab02f24e1973f
Oliver Eidel <oli@eidel.net>
2020-03-22 00:31:19 +0100

    include js files (mostly stimulus controllers) in purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -17,7 +17,7 @@ const environment = {
 if (process.env.RAILS_ENV !== 'development') {
   environment.plugins.push(
     require('@fullhuman/postcss-purgecss')({
-      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
+      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb', './app/**/*.js'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
   );
