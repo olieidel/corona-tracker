b8052c1d04a4e352335565d5cd610a45f42411e9
Oliver Eidel <oli@eidel.net>
2020-03-25 12:21:21 +0100

    change sliding window to 14 days

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Corona
     # the framework and any gems in your application.
     config.mapbox_public_key = "pk.eyJ1Ijoib2xpZWlkZWwiLCJhIjoiY2s3Z2J1bG1lMDlidjNtbzI1MGVlM3czOCJ9.fAXDKOEHx_USnDXah1iENw"
 
-    config.sliding_window = 72.hours
+    config.sliding_window = 14.days
     config.default_heatmap_radius_km = 20
   end
 end
